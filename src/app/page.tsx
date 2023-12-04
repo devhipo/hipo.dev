31680a89d6023f53441f2fd1f336125066c3549b
Fırat KÜÇÜK <firatkucuk@gmail.com>
2023-12-04 22:58:22 +0100

    logo added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,348 @@
 export default function Home() {
   return (
-    <div>
-      <h1>hipo.dev</h1>
-    </div>
-  )
+    <>
+      <header className="text-gray-600 body-font bg-hipo-bg">
+        <div className="container mx-auto flex px-10 p-3 flex-col md:flex-row items-center justify-between">
+          <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0" href="/">
+            <img alt="hipo.dev" className="h-10" src="img/horizontal-logo-regular-black.svg"/>
+          </a>
+          <a href="https://github.com/devhipo/hipo" target="_blank">
+            <svg className="h-6 fill-gray-100" version="1.1" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+              <path d="M296.133,354.174c49.885-5.891,102.942-24.029,102.942-110.192 c0-24.49-8.624-44.448-22.67-59.869c2.266-5.89,9.515-28.114-2.734-58.947c0,0-18.139-5.898-60.759,22.669 c-18.139-4.983-38.09-8.163-56.682-8.163c-19.053,0-39.011,3.18-56.697,8.163c-43.082-28.567-61.22-22.669-61.22-22.669 c-12.241,30.833-4.983,53.057-2.718,58.947c-14.061,15.42-22.677,35.379-22.677,59.869c0,86.163,53.057,104.301,102.942,110.192 c-6.344,5.452-12.241,15.873-14.507,30.387c-12.702,5.438-45.808,15.873-65.758-18.592c0,0-11.795-21.31-34.012-22.669 c0,0-22.224-0.453-1.813,13.592c0,0,14.96,6.812,24.943,32.653c0,0,13.6,43.089,76.179,29.48v38.543 c0,5.906-4.53,12.702-15.865,10.89C96.139,438.977,32.2,354.626,32.2,255.77c0-123.807,100.216-224.022,224.03-224.022 c123.347,0,224.023,100.216,223.57,224.022c0,98.856-63.946,182.754-152.828,212.688c-11.342,2.266-15.873-4.53-15.873-10.89 V395.45C311.1,374.577,304.288,360.985,296.133,354.174L296.133,354.174z M512,256.23C512,114.73,397.263,0,256.23,0 C114.73,0,0,114.73,0,256.23C0,397.263,114.73,512,256.23,512C397.263,512,512,397.263,512,256.23L512,256.23z"/>
+            </svg>
+          </a>
+        </div>
+      </header>
+
+      <section className="text-gray-400 bg-hipo-bg2 body-font px-36 p-24">
+        <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+          <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+              Java Execution Redefined
+            </h1>
+            <p className="mb-8 leading-relaxed">
+              No download, no installation. Just type the Maven <strong>group id</strong> and <strong>artifact id</strong>.
+            </p>
+          </div>
+          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+            <div className="font-code p-5 bg-black border border-gray-600 rounded-lg">
+              <div className="mb-4 flex gap-1.5">
+                <svg className="h-3 fill-stone-700" version="1.1" viewBox="0 0 16 16"  xmlns="http://www.w3.org/2000/svg"><circle cx="8" cy="8" r="8"/></svg>
+                <svg className="h-3 fill-stone-700" version="1.1" viewBox="0 0 16 16"  xmlns="http://www.w3.org/2000/svg"><circle cx="8" cy="8" r="8"/></svg>
+                <svg className="h-3 fill-stone-700" version="1.1" viewBox="0 0 16 16"  xmlns="http://www.w3.org/2000/svg"><circle cx="8" cy="8" r="8"/></svg>
+              </div>
+              {/* eslint-disable-next-line react/no-unescaped-entities */}
+              <span className="text-teal-800">curl</span> -s <span className="text-yellow-600">"https://get.hipo.dev"</span> | bash<br />
+              <span className="text-teal-800">hipo</span> dev.hipo:hello<br/><br/>
+
+              Hello Hipo!
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="text-gray-400 bg-hipo-bg3 body-font px-12">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="text-center mb-20">
+            <h1 className="sm:text-3xl text-2xl font-medium title-font text-white mb-4">
+              Raw Denim Heirloom Man Braid
+            </h1>
+            <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-400 text-opacity-80">
+              Blue bottle crucifix vinyl post-ironic four dollar toast vegan
+              taxidermy. Gastropub indxgo juice poutine, ramps microdosing banh
+              mi pug.
+            </p>
+            <div className="flex mt-6 justify-center">
+              <div className="w-16 h-1 rounded-full bg-yellow-500 inline-flex"></div>
+            </div>
+          </div>
+          <div className="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6">
+            <div className="p-4 md:w-1/3 flex flex-col text-center items-center">
+              <div className="w-20 h-20 inline-flex items-center justify-center rounded-full bg-gray-800 text-yellow-400 mb-5 flex-shrink-0">
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  className="w-10 h-10"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+                </svg>
+              </div>
+              <div className="flex-grow">
+                <h2 className="text-white text-lg title-font font-medium mb-3">
+                  Shooting Stars
+                </h2>
+                <p className="leading-relaxed text-base">
+                  Blue bottle crucifix vinyl post-ironic four dollar toast vegan
+                  taxidermy. Gastropub indxgo juice poutine, ramps microdosing
+                  banh mi pug VHS try-hard.
+                </p>
+                <a className="mt-3 text-yellow-400 inline-flex items-center">
+                  Learn More
+                  <svg
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    className="w-4 h-4 ml-2"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M5 12h14M12 5l7 7-7 7"></path>
+                  </svg>
+                </a>
+              </div>
+            </div>
+            <div className="p-4 md:w-1/3 flex flex-col text-center items-center">
+              <div className="w-20 h-20 inline-flex items-center justify-center rounded-full bg-gray-800 text-yellow-400 mb-5 flex-shrink-0">
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  className="w-10 h-10"
+                  viewBox="0 0 24 24"
+                >
+                  <circle cx="6" cy="6" r="3"></circle>
+                  <circle cx="6" cy="18" r="3"></circle>
+                  <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"></path>
+                </svg>
+              </div>
+              <div className="flex-grow">
+                <h2 className="text-white text-lg title-font font-medium mb-3">
+                  The Catalyzer
+                </h2>
+                <p className="leading-relaxed text-base">
+                  Blue bottle crucifix vinyl post-ironic four dollar toast vegan
+                  taxidermy. Gastropub indxgo juice poutine, ramps microdosing
+                  banh mi pug VHS try-hard.
+                </p>
+                <a className="mt-3 text-yellow-400 inline-flex items-center">
+                  Learn More
+                  <svg
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    className="w-4 h-4 ml-2"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M5 12h14M12 5l7 7-7 7"></path>
+                  </svg>
+                </a>
+              </div>
+            </div>
+            <div className="p-4 md:w-1/3 flex flex-col text-center items-center">
+              <div className="w-20 h-20 inline-flex items-center justify-center rounded-full bg-gray-800 text-yellow-400 mb-5 flex-shrink-0">
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  className="w-10 h-10"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+                  <circle cx="12" cy="7" r="4"></circle>
+                </svg>
+              </div>
+              <div className="flex-grow">
+                <h2 className="text-white text-lg title-font font-medium mb-3">
+                  Neptune
+                </h2>
+                <p className="leading-relaxed text-base">
+                  Blue bottle crucifix vinyl post-ironic four dollar toast vegan
+                  taxidermy. Gastropub indxgo juice poutine, ramps microdosing
+                  banh mi pug VHS try-hard.
+                </p>
+                <a className="mt-3 text-yellow-400 inline-flex items-center">
+                  Learn More
+                  <svg
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    className="w-4 h-4 ml-2"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M5 12h14M12 5l7 7-7 7"></path>
+                  </svg>
+                </a>
+              </div>
+            </div>
+          </div>
+          <button className="flex mx-auto mt-16 text-white bg-yellow-500 border-0 py-2 px-8 focus:outline-none hover:bg-yellow-600 rounded text-lg">
+            Button
+          </button>
+        </div>
+      </section>
+
+      <section className="text-gray-400  bg-hipo-bg2 body-font px-12">
+        <div className="container px-5 py-24 mx-auto flex flex-wrap">
+          <div className="flex flex-wrap w-full">
+            <div className="lg:w-2/5 md:w-1/2 md:pr-10 md:py-6">
+              <div className="flex relative pb-12">
+                <div className="h-full w-10 absolute inset-0 flex items-center justify-center">
+                  <div className="h-full w-1 bg-gray-800 pointer-events-none"></div>
+                </div>
+                <div className="flex-shrink-0 w-10 h-10 rounded-full bg-yellow-500 inline-flex items-center justify-center text-white relative z-10">
+                  <svg
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    className="w-5 h-5"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
+                  </svg>
+                </div>
+                <div className="flex-grow pl-4">
+                  <h2 className="font-medium title-font text-sm text-white mb-1 tracking-wider">
+                    STEP 1
+                  </h2>
+                  <p className="leading-relaxed">
+                    VHS cornhole pop-up, try-hard 8-bit iceland helvetica.
+                    Kinfolk bespoke try-hard cliche palo santo offal.
+                  </p>
+                </div>
+              </div>
+              <div className="flex relative pb-12">
+                <div className="h-full w-10 absolute inset-0 flex items-center justify-center">
+                  <div className="h-full w-1 bg-gray-800 pointer-events-none"></div>
+                </div>
+                <div className="flex-shrink-0 w-10 h-10 rounded-full bg-yellow-500 inline-flex items-center justify-center text-white relative z-10">
+                  <svg
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    className="w-5 h-5"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+                  </svg>
+                </div>
+                <div className="flex-grow pl-4">
+                  <h2 className="font-medium title-font text-sm text-white mb-1 tracking-wider">
+                    STEP 2
+                  </h2>
+                  <p className="leading-relaxed">
+                    Vice migas literally kitsch +1 pok pok. Truffaut hot chicken
+                    slow-carb health goth, vape typewriter.
+                  </p>
+                </div>
+              </div>
+              <div className="flex relative pb-12">
+                <div className="h-full w-10 absolute inset-0 flex items-center justify-center">
+                  <div className="h-full w-1 bg-gray-800 pointer-events-none"></div>
+                </div>
+                <div className="flex-shrink-0 w-10 h-10 rounded-full bg-yellow-500 inline-flex items-center justify-center text-white relative z-10">
+                  <svg
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    className="w-5 h-5"
+                    viewBox="0 0 24 24"
+                  >
+                    <circle cx="12" cy="5" r="3"></circle>
+                    <path d="M12 22V8M5 12H2a10 10 0 0020 0h-3"></path>
+                  </svg>
+                </div>
+                <div className="flex-grow pl-4">
+                  <h2 className="font-medium title-font text-sm text-white mb-1 tracking-wider">
+                    STEP 3
+                  </h2>
+                  <p className="leading-relaxed">
+                    Coloring book nar whal glossier master cleanse umami. Salvia
+                    +1 master cleanse blog taiyaki.
+                  </p>
+                </div>
+              </div>
+              <div className="flex relative pb-12">
+                <div className="h-full w-10 absolute inset-0 flex items-center justify-center">
+                  <div className="h-full w-1 bg-gray-800 pointer-events-none"></div>
+                </div>
+                <div className="flex-shrink-0 w-10 h-10 rounded-full bg-yellow-500 inline-flex items-center justify-center text-white relative z-10">
+                  <svg
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    className="w-5 h-5"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+                    <circle cx="12" cy="7" r="4"></circle>
+                  </svg>
+                </div>
+                <div className="flex-grow pl-4">
+                  <h2 className="font-medium title-font text-sm text-white mb-1 tracking-wider">
+                    STEP 4
+                  </h2>
+                  <p className="leading-relaxed">
+                    VHS cornhole pop-up, try-hard 8-bit iceland helvetica.
+                    Kinfolk bespoke try-hard cliche palo santo offal.
+                  </p>
+                </div>
+              </div>
+              <div className="flex relative">
+                <div className="flex-shrink-0 w-10 h-10 rounded-full bg-yellow-500 inline-flex items-center justify-center text-white relative z-10">
+                  <svg
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    className="w-5 h-5"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
+                    <path d="M22 4L12 14.01l-3-3"></path>
+                  </svg>
+                </div>
+                <div className="flex-grow pl-4">
+                  <h2 className="font-medium title-font text-sm text-white mb-1 tracking-wider">
+                    FINISH
+                  </h2>
+                  <p className="leading-relaxed">
+                    Pitchfork ugh tattooed scenester echo park gastropub
+                    whatever cold-pressed retro.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <img
+              className="lg:w-3/5 md:w-1/2 object-cover object-center rounded-lg md:mt-0 mt-12"
+              src="https://dummyimage.com/1200x500"
+              alt="step"
+            />
+          </div>
+        </div>
+      </section>
+
+      <footer className="text-gray-400 bg-black body-font px-12">
+        <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
+          <a className="flex title-font font-medium items-center md:justify-start justify-center text-white" href="/">
+            <img alt="hipo.dev" className="h-8" src="img/horizontal-logo-light-tr.svg"/>
+          </a>
+          <p className="text-sm text-gray-400 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-800 sm:py-2 sm:mt-0 mt-4">
+            © 2023 hipo.dev
+          </p>
+        </div>
+      </footer>
+    </>
+  );
 }
